19b245b2015e3c1e0b7c6a212948422d58ac8f34
Laurent Gatto <laurent.gatto@uclouvain.be>
2022-08-10 16:04:37 +0200

    build book

--- install_docs_deps.R
+++ install_docs_deps.R
@@ -1,9 +1,11 @@
-deps <- c("BiocStyle", "dplyr", "factoextra", "ggplot2", "gplots",
-          "limma", "magrittr", "MsCoreUtils", "msdata", "MSnID",
-          "mzID", "mzR", "patchwork", "PSMatch", "QFeatures", "rpx",
-          "Spectra", "tidyr", "tidyverse", "lgatto/msmbstyle")
-
 if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
 
+deps <- c("dplyr", "factoextra", "ggplot2", "gplots", "limma",
+          "magrittr", "MsCoreUtils", "msdata", "MSnID", "mzID", "mzR",
+          "patchwork", "PSMatch", "QFeatures", "rpx", "Spectra",
+          "tidyr", "tidyverse")
 BiocManager::install(deps, ask = FALSE, udpate = TRUE)
+
+deps2 <- c("lgatto/msmbstyle", "BiocStyle", "bookdown")
+BiocManager::install(deps2, ask = FALSE, udpate = TRUE)
