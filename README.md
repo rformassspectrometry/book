e4558af18e6a53cc8505eb23f3f4fea93d6ab586
js2264 <jacquesserizay@gmail.com>
2023-11-06 17:17:14 +0100

    Init BiocBook from template version 1.0.5

---
+++ README.md
@@ -0,0 +1,5 @@
+<!-- badges: start -->
+📦 [Repo](https://github.com/js2264/R4MS) [![rworkflows](https://img.shields.io/github/actions/workflow/status/js2264/R4MS/rworkflows.yml?label=Package%20check)](https://github.com/js2264/R4MS/actions/workflows/rworkflows.yml)   
+📖 [Book](https://js2264.github.io/R4MS/devel) [![deployment](https://img.shields.io/github/actions/workflow/status/js2264/R4MS/pages/pages-build-deployment?label=Book%20deployment)](https://github.com/js2264/R4MS/actions/workflows/pages/pages-build-deployment)  
+🐳 [Docker](https://github.com/js2264/R4MS/pkgs/container/R4MS) [![biocbook](https://img.shields.io/github/actions/workflow/status/js2264/R4MS/biocbook.yml?label=Docker%20image)](https://github.com/js2264/R4MS/actions/workflows/biocbook.yml)  
+<!-- badges: end -->
---
+++ inst/extensions/tools-tabset-ext/tools-tabset.lua
@@ -0,0 +1,39 @@
+
+
+local kTabsetIcons = {
+  ["VS Code"] = "vscode-logo.jpg",
+  ["R"] = "rstudio-logo.jpg",
+  ["Terminal"] = "text-editor-logo.jpg"
+}
+
+local injected = false
+local function injectChooseYourTool()
+  if not injected then
+    injected = true
+    quarto.doc.include_text('after-body', [[
+      <script type="text/javascript">
+        for (const navTab of document.querySelectorAll(".panel-tabset[data-group='tools-tabset'] ul")) {
+          const choose = document.createElement("h3");
+          choose.classList.add("no-anchor");
+          choose.classList.add("choose-your-tool");
+          choose.innerText = "Choose your tool";
+          navTab.prepend(choose);
+        }       
+      </script>
+    ]])
+  end
+end
+
+function Tabset(el)
+  if el.attr.attributes["group"] == "tools-tabset" then
+    injectChooseYourTool()
+    for i, tab in ipairs(el.tabs) do
+      local text = pandoc.utils.stringify(tab.title)
+      local icon = kTabsetIcons[text]
+      if icon then
+        tab.title.content:insert(1, pandoc.Image("", "/pages/images/" .. icon))
+      end
+    end
+  end
+  return el
+end
